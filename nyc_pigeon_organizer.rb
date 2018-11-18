d2e06d6e3fe617193bd628891e96de9248b13b9b
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 07:38:26 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,25 +1,25 @@
-# require 'pry'
-# def nyc_pigeon_organizer(data)
-#   # write your code here!
-#   new_hash = {}
-#   data.each do |cgl, pdata|
-# #   binding.pry
-#     pdata.each do |identifier, name|
-#       for i in 0...name.length
-# #     binding.pry
-#         if new_hash[name[i]] == nil
-#           new_hash[name[i]] = [cgl]
-#         else
-#           new_hash[name[i]] += [cgl]
-#         end
-#       end
-#     end
-#   end
-#   new_hash.each do |name, cgl|
-#     new_hash[name] = cgl.uniq
+require 'pry'
+def nyc_pigeon_organizer(data)
+  # write your code here!
+  new_hash = {}
+  data.each do |cgl, pdata|
+#   binding.pry
+    pdata.each do |identifier, name|
+      for i in 0...name.length
 #     binding.pry
-#   end
-# end
+        if new_hash[name[i]] == nil
+          new_hash[name[i]] = [cgl]
+        else
+          new_hash[name[i]] += [cgl]
+        end
+      end
+    end
+  end
+  new_hash.each do |name, cgl|
+    new_hash[name] = cgl.uniq
+    binding.pry
+  end
+end
 
 
 def nyc_pigeon_organizer(pigeon_data)
@@ -50,12 +50,3 @@ def nyc_pigeon_organizer(pigeon_data)
       end
   end
  pigeon_list
-
-
-# {"Theo"=>[:color, :gender, :lives],
-# "Peter Jr."=>[:color, :gender, :lives],
-# "Lucky"=>[:color, :gender, :lives],
-# "Ms. K"=>[:color, :gender, :lives],
-# "Queenie"=>[:color, :gender, :lives],
-# "Andrew"=>[:color, :gender, :lives],
-# "Alex"=>[:color, :gender, :lives]}
