10672d251711008b3665c8998b465eb37dc6961c
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 07:41:24 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,31 +15,31 @@ def nyc_pigeon_organizer(data)
 end
 
 
-# def nyc_pigeon_organizer(pigeon_data)
-#   # write your code here!
-#   pigeon_list = {}
-#   pigeon_data.each do |color_gender_lives, facts|
-#     facts.each do |facts_key, array_names|
+def nyc_pigeon_organizer(pigeon_data)
+  # write your code here!
+  pigeon_list = {}
+  pigeon_data.each do |color_gender_lives, facts|
+    facts.each do |facts_key, array_names|
     
     
-#       array_names.each do |names|
-#       pigeon_list[names] = {}
-#       end
-#     end
-#   end
-#   pigeon_data.each do |color_gender_lives, facts|
-#     pigeon_list.keys.each do |pigeon_name|
-#       pigeon_list[pigeon_name][color_gender_lives] = []
+      array_names.each do |names|
+      pigeon_list[names] = {}
+      end
+    end
+  end
+  pigeon_data.each do |color_gender_lives, facts|
+    pigeon_list.keys.each do |pigeon_name|
+      pigeon_list[pigeon_name][color_gender_lives] = []
     
    
-#       facts.each do |facts_key, array_names|
+      facts.each do |facts_key, array_names|
     
     
-#         if array_names.include? (pigeon_name)
+        if array_names.include? (pigeon_name)
        
-#         pigeon_list[pigeon_name][color_gender_lives].push(facts_key.to_s)
-#         end
-#         end
-#       end
-#   end
-# pigeon_list
+        pigeon_list[pigeon_name][color_gender_lives].push(facts_key.to_s)
+        end
+        end
+      end
+  end
+pigeon_list
