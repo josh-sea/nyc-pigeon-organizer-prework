c203829a007af4424dc27db1c3f11bda1f3ed4c7
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 08:10:28 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -33,9 +33,9 @@ def nyc_pigeon_organizer(data)
       new_hash[i][cgl] = []
  #     binding.pry
       pdata.each do |identifier, name|
-        binding.pry
+ #       binding.pry
         if name.include?(i)
-          binding.pry
+#          binding.pry
           new_hash[names][cgl].push(keys.to_s)
         end
       end
