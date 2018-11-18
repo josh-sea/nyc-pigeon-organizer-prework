2c6fde06f8061f404c0e349ad66d6d9b476d3709
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 07:50:11 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -21,7 +21,7 @@ def nyc_pigeon_organizer(data)
       new_hash[names][cgl] = []
       identifier.each do |keys, name_arrays|
         if name_arrays.include? (pigeon_name)
-        new_hasht[names][cgl].push(facts_key.to_s)
+        new_hash[names][cgl].push(keys.to_s)
         end
       end
     end
@@ -30,29 +30,29 @@ def nyc_pigeon_organizer(data)
 end
 
 
-def nyc_pigeon_organizer(pigeon_data)
-  # write your code here!
-  pigeon_list = {}
-  pigeon_data.each do |color_gender_lives, facts|
-    facts.each do |facts_key, array_names|
-      array_names.each do |names|
-      pigeon_list[names] = {}
-      end
-    end
-  end
-  pigeon_data.each do |color_gender_lives, facts|
-    pigeon_list.keys.each do |pigeon_name|
-      pigeon_list[pigeon_name][color_gender_lives] = []
+# def nyc_pigeon_organizer(pigeon_data)
+#   # write your code here!
+#   pigeon_list = {}
+#   pigeon_data.each do |color_gender_lives, facts|
+#     facts.each do |facts_key, array_names|
+#       array_names.each do |names|
+#       pigeon_list[names] = {}
+#       end
+#     end
+#   end
+#   pigeon_data.each do |color_gender_lives, facts|
+#     pigeon_list.keys.each do |pigeon_name|
+#       pigeon_list[pigeon_name][color_gender_lives] = []
     
    
-      facts.each do |facts_key, array_names|
+#       facts.each do |facts_key, array_names|
     
     
-        if array_names.include? (pigeon_name)
+#         if array_names.include? (pigeon_name)
        
-        pigeon_list[pigeon_name][color_gender_lives].push(facts_key.to_s)
-        end
-        end
-      end
-  end
-pigeon_list
+#         pigeon_list[pigeon_name][color_gender_lives].push(facts_key.to_s)
+#         end
+#         end
+#       end
+#   end
+# pigeon_list
